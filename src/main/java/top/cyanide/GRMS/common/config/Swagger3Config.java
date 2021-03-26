ce8c37afd456d35fde984367d30f84f0ea188921
xiaobai <247374593@qq.com>
2021-03-26 14:00:06 +0800

    refact

--- src/main/java/top/cyanide/GRMS/common/config/Swagger3Config.java
+++ src/main/java/top/cyanide/GRMS/common/config/Swagger3Config.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
@@ -15,6 +16,7 @@ import springfox.documentation.spring.web.plugins.Docket;
  * @author Bai
  */
 @Configuration
+@EnableOpenApi
 public class Swagger3Config {
     @Bean
     public Docket createRestApi() {
